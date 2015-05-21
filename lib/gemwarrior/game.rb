913e4592c23bfa0ae6ef55cac2e6a7c27643a0a5
Michael Chadwick <michael.chadwick@gmail.com>
2015-05-21 11:54:18 -0700

    instance vars to methods

--- lib/gemwarrior/game.rb
+++ lib/gemwarrior/game.rb
@@ -21,10 +21,12 @@ module Gemwarrior
     PLYR_ATK_HI_DEFAULT = 2
     PLYR_ROX_DEFAULT = 0
 
+    attr_accessor :world, :eval, :repl
+    
     def initialize
       # create new world and player
-      @world = World.new
-      @world.player = Player.new(
+      self.world = World.new
+      world.player = Player.new(
         PLYR_LEVEL_DEFAULT,
         PLYR_XP_DEFAULT,
         PLYR_HP_CUR_DEFAULT,
@@ -35,15 +37,15 @@ module Gemwarrior
         PLYR_ATK_HI_DEFAULT,
         Inventory.new,
         PLYR_ROX_DEFAULT,
-        @world.loc_by_id(0)
+        world.loc_by_id(0)
       )
 
       # create the console
-      @eval = Evaluator.new(@world)
-      @repl = Repl.new(@world, @eval)
+      self.eval = Evaluator.new(@world)
+      self.repl = Repl.new(@world, @eval)
 
       # enter Jool!
-      @repl.start('look')
+      repl.start('look')
     end
 
   end
