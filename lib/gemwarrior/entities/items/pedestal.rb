02cab7197f6131bc3d355c3226209fd20bf0abad
Michael Chadwick <michael.chadwick@gmail.com>
2015-08-10 23:50:33 -0700

    pedestal now only allows rearrangements of original jumbled letters and not other characters

--- lib/gemwarrior/entities/items/pedestal.rb
+++ lib/gemwarrior/entities/items/pedestal.rb
@@ -39,7 +39,7 @@ module Gemwarrior
         print_switches(self.switches)
         
         puts 
-        puts 'You can change the words themselves and you can push the large gem. What do you do?'
+        puts 'You can rearrange the letters in the words themselves, and you can push the large gem. What do you do?'
         puts
         puts '>> 1 Rearrange the letters in switch 1\'s word'
         puts '>> 2 Rearrange the letters in switch 2\'s word'
@@ -53,49 +53,43 @@ module Gemwarrior
         
         print '> '
         choice = gets.chomp!
-        
+
         case choice
         when '1'
           switch1 = switches[0][2].nil? ? switches[0][1] : switches[0][2]
           puts "Switch 1: #{switch1}"
           puts 'What should the switch\'s word be set to?'
-          new_word = gets.chomp!
-          switches[0][2] = new_word
+          switches[0][2] = get_new_word(switch1)
         when '2'
           switch2 = switches[1][2].nil? ? switches[1][1] : switches[1][2]
           puts "Switch 2: #{switch2}"
           puts 'What should the switch\'s word be set to?'
-          new_word = gets.chomp!
-          switches[1][2] = new_word
+          switches[1][2] = get_new_word(switch2)
         when '3'
           switch3 = switches[2][2].nil? ? switches[2][1] : switches[2][2]
           puts "Switch 3: #{switch3}"
           puts 'What should the switch\'s word be set to?'
-          new_word = gets.chomp!
-          switches[2][2] = new_word
+          switches[2][2] = get_new_word(switch3)
         when '4'
           switch4 = switches[3][2].nil? ? switches[3][1] : switches[3][2]
           puts "Switch 4: #{switch4}"
           puts 'What should the switch\'s word be set to?'
-          new_word = gets.chomp!
-          switches[3][2] = new_word
+          switches[3][2] = get_new_word(switch4)
         when '5'
           switch5 = switches[4][2].nil? ? switches[4][1] : switches[4][2]
           puts "Switch 5: #{switch5}"
           puts 'What should the switch\'s word be set to?'
-          new_word = gets.chomp!
-          switches[4][2] = new_word
+          switches[4][2] = get_new_word(switch5)
         when '6'
           switch6 = switches[5][2].nil? ? switches[5][1] : switches[5][2]
           puts "Switch 6: #{switch6}"
           puts 'What should the switch\'s word be set to?'
-          new_word = gets.chomp!
-          switches[5][2] = new_word
+          switches[5][2] = get_new_word(switch6)
         when '7'
           pedestal_configured = true
       
           for i in 0..switches.length-1 do
-            pedestal_configured = false unless switches[i][0].eql? switches[i][2]
+            pedestal_configured = false unless switches[i][0].downcase.eql? switches[i][2].downcase
           end   
       
           if pedestal_configured
@@ -144,6 +138,16 @@ module Gemwarrior
     
     def jumble(word)
       word.split(//).sort_by { rand }.join('')
+    end
+    
+    def get_new_word(switch)
+      new_word = gets.chomp!
+      if switch.downcase.split(//).sort == new_word.downcase.split(//).sort
+        return new_word
+      else
+        puts 'Those letters are not in the original word, sorry.'.colorize(:red)
+        return nil
+      end
     end
   end
 end
--- lib/gemwarrior/version.rb
+++ lib/gemwarrior/version.rb
@@ -2,5 +2,5 @@
 # Version of Gem Warrior
 
 module Gemwarrior
-  VERSION = '0.9.21'
+  VERSION = '0.9.22'
 end
