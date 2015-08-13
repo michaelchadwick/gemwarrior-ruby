7ec96a9bc699d803a441bd1e8a7bcc5c20a10972
Michael Chadwick <michael.chadwick@gmail.com>
2015-08-13 11:01:36 -0700

    set up some basic, failed tests

--- spec/gemwarrior_spec.rb
+++ spec/gemwarrior_spec.rb
@@ -1,7 +1,42 @@
 require 'spec_helper'
+include Gemwarrior
 
 describe Gemwarrior do
-  it 'has a version number' do
-    expect(Gemwarrior::VERSION).not_to be nil
+  let(:default_load_options) do
+    GameOptions.add 'log_file_path', "#{Dir.home}/.gemwarrior_log"
+    GameOptions.add 'options_file_path', "#{Dir.home}/.gemwarrior_options"
+  
+    Game.new(
+      beast_mode:     false,
+      debug_mode:     false,
+      god_mode:       false,
+      new_game:       false,
+      sound_enabled:  false,
+      sound_volume:   0.3,
+      use_wordnik:    false,
+      extra_command:  nil
+    )
+  end
+
+  describe 'basic properties' do
+    it 'has a version number' do
+      expect(VERSION).not_to be nil
+    end
+    it 'display help menu' do
+      pending
+    end
+  end
+  
+  describe 'game load' do
+    context 'default options' do
+      it 'displays the main menu' do
+        pending
+      end
+    end
+    context 'new game option enabled' do
+      it 'displays the main game prompt and runs the look command' do
+        pending
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+# binary/executable
+$LOAD_PATH.unshift File.expand_path('../../bin', __FILE__)
+# main module
 $LOAD_PATH.unshift File.expand_path('../../lib/gemwarrior', __FILE__)
 require 'pry'
 require 'game'
