8f81a81f4804a2aca8af8d33ee0ffe329ee24b07
Michael Chadwick <michael.chadwick@gmail.com>
2015-08-13 09:53:16 -0700

    basic rspec test works now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib/gemwarrior', __FILE__)
 require 'pry'
-require 'gemwarrior'
+require 'game'
